aa38d1f504b781c71ba317ba40e019537ad4a773
thdwlsgus0 <thdwlsgus0@naver.com>
2021-05-02 23:25:52 +0900

    feat: 원 생성 완료

--- js/circle.js
+++ js/circle.js
@@ -2,17 +2,33 @@ class Circle {
   constructor(){
     this.x = Math.floor(Math.random() * 1000 + 1);
     this.y = Math.floor(Math.random() * 1000 + 1);
-    this.radius = Math.floor(Math.random() * 100 + 1);
+    this.RADIUS = 50;
+  }
+
+  create() {
+    const canvasElement = document.createElement('canvas');
+    const context = canvasElement.getContext('2d');
+    context.beginPath();
+    context.arc(this.x,this.y,this.RADIUS,0,Math.PI*2);
+    context.stroke();
+    document.body.appendChild(canvasElement);
   }
 
   move(){
 
   }
 }
+const array = [];
+const CIRCLE_NUMBER = 30;
 
-const array = new Array(100);
-
-for(let i=0; i<10; i++){
-  const arr = new Circle();
-  console.log(arr);
+for(let i=0; i<CIRCLE_NUMBER; i++){
+  const circle = new Circle();
+  circle.create();
 }
+
+const screenSize = () => {
+
+}
+window.addEventListener('resize', )
+
+
